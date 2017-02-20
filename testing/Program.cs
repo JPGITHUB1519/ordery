72e3f1b80a870deefdda9e0a8835a1d86a4f0b20
Jean Ureña <juanpedro1519@gmail.com>
2017-02-19 21:29:47 -0300

    Add Security Class

--- testing/Program.cs
+++ testing/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using utils;
 namespace testing
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string texto = "hola";
+            string encriptado = Security.Encrypt(texto);
+            string desencriptado = Security.decrypting(encriptado);
+
+            Console.WriteLine("Encriptado : " + encriptado);
+            Console.WriteLine("Desencriptado : " + desencriptado);
+            Console.ReadKey();
+
         }
     }
 }
---
+++ utils/Security.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils
+{
+    /* Clase para cosas de seguridad como Encriptar / Desencriptar */
+    public class Security
+    {
+        // Encrypt
+        public static string Encrypt(string text)
+        {
+            string result = String.Empty;
+            byte[] encrypted = System.Text.Encoding.Unicode.GetBytes(text);
+            result = Convert.ToBase64String(encrypted);
+            return result;
+        }
+        
+        // Decrypting
+        public static string decrypting(string encrypted)
+        {
+            string result = String.Empty;
+            byte[] decrypted = Convert.FromBase64String(encrypted);
+            result = System.Text.Encoding.Unicode.GetString(decrypted);
+            return result;
+        }
+    }
+}
